36442f2a2cdebda0991c72bcb3ec30a1037219f2
Michael Kalantar <kalantar@us.ibm.com>
2019-07-20 14:00:25 -0400

    fix master pipeline

--- scripts/create_experiment.sh
+++ scripts/create_experiment.sh
@@ -27,7 +27,7 @@ fi
 # Update experiment update
 yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} metadata.name ${EXPERIMENT_NAME}
 yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} spec.targetService.baseline ${BASELINE_DEPLOYMENT_NAME}
-yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} spec.targetService.candidate ${CANARY_VERSION}
+yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} spec.targetService.candidate ${CANARY_DEPLOYMENT_NAME}
 yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} spec.trafficControl.onSuccess ${ON_SUCCESS}
 
 cat ${EXPERIMENT_TEMPLATE_FILE}
