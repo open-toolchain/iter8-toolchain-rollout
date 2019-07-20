9ac2d2dd4305eb2fbd1cdf5d837e0a586934a8dc
Michael Kalantar <kalantar@us.ibm.com>
2019-07-20 10:43:42 -0400

    update stable pipeline

---
+++ scripts/create_deployment.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# uncomment to debug the script
+# set -x
+# copy the script below into your app code repo (e.g. ./scripts/create_deployment.sh) and 'source' it from your pipeline job
+#    source ./scripts/create_deployment.sh
+# alternatively, you can source it from online script:
+#    source <(curl -sSL "https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/create_deployment.sh")
+# ------------------
+# source: https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/create_deployment.sh
+
+# Create canary deployment yaml
+
+
+export DEPLOYMENT_FILE=deploy.yaml
+
+if [ -z "${PATCH_FILE}" ]; then PATCH_FILE=kustomize_patch.yaml ; fi
+if [ ! -f ${PATCH_FILE} ]; then
+    echo -e "${red}kustomize patch file '${PATCH_FILE}' not found${no_color}"
+fi
+
+# Install kustomize
+opsys=linux  # or darwin, or windows
+curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases/latest |\
+    grep browser_download |\
+    grep $opsys |\
+    cut -d '"' -f 4 |\
+    xargs curl -O -L
+mv kustomize_*_${opsys}_amd64 /usr/local/bin/kustomize
+chmod u+x /usr/local/bin/kustomize
+
+# Modify patch file using $IMAGE_TAG as VERSION
+sed -i -e "s#iter8/reviews:istio-VERSION#$PIPELINE_IMAGE_URL#" ${PATCH_FILE}
+sed -i -e "s#VERSION#${IMAGE_TAG}#g" ${PATCH_FILE}
+cat ${PATCH_FILE}
+
+# Create deployment yaml with kustomize build
+kustomize build kustomize -o ${DEPLOYMENT_FILE}
--- scripts/create_experiment.sh
+++ scripts/create_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # uncomment to debug the script
 # set -x
-# copy the script below into your app code repo (e.g. ./scripts/istio_check_install.sh) and 'source' it from your pipeline job
+# copy the script below into your app code repo (e.g. ./scripts/create_experiment.sh) and 'source' it from your pipeline job
 #    source ./scripts/create_experiment.sh
 # alternatively, you can source it from online script:
 #    source <(curl -sSL "https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/create_experiment.sh")
@@ -11,34 +11,26 @@
 # Create iter8 experiment in target cluster/namespace
 
 # Input env variables from pipeline job
-echo "PIPELINE_KUBERNETES_CLUSTER_NAME=${PIPELINE_KUBERNETES_CLUSTER_NAME}"
-echo "CLUSTER_NAMESPACE=${CLUSTER_NAMESPACE}"
-echo "IMAGE_NAME=${IMAGE_NAME}"
-echo "IMAGE_TAG=${IMAGE_TAG}"
-echo "BASELINE_VERSION=${BASELINE_VERSION}"
-echo "CANARY_DEPLOYMENT_NAME=${CANARY_DEPLOYMENT_NAME}"
 echo "EXPERIMENT_TEMPLATE_FILE=${EXPERIMENT_TEMPLATE_FILE}"
+echo "EXPERIMENT_NAME=${EXPERIMENT_NAME}"
+echo "BASELINE_DEPLOYMENT_NAME=${BASELINE_DEPLOYMENT_NAME}"
+echo "CANARY_DEPLOYMENT_NAME=${CANARY_DEPLOYMENT_NAME}"
+echo "ON_SUCCESS=${ON_SUCCESS}"
+echo "CLUSTER_NAMESPACE=${CLUSTER_NAMESPACE}"
 
+# Fail early if no experient template
 if [ -z "${EXPERIMENT_TEMPLATE_FILE}" ]; then EXPERIMENT_TEMPLATE_FILE=iter8_experiment.yaml ; fi
 if [ ! -f ${EXPERIMENT_TEMPLATE_FILE} ]; then
   echo -e "${red}iter8 experiment template '${EXPERIMENT_TEMPLATE_FILE}' not found${no_color}"
 fi
 
-#WOW: APP_NAME=$( cat ${DEPLOYMENT_FILE} | yq r - -j | jq -r '. | select(.kind=="Deployment") | if (.metadata.labels.app) then .metadata.labels.app else .metadata.name end' )
-NAME=$(yq read ${EXPERIMENT_TEMPLATE_FILE} metadata.name)
-# export experimet name so can later patch it
-export EXPERIMENT_NAME=${NAME}-${BUILD_NUMBER}
-echo "EXPERIMENT_NAME=${EXPERIMENT_NAME}"
+# Update experiment update
+yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} metadata.name ${EXPERIMENT_NAME}
+yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} spec.targetService.baseline ${BASELINE_DEPLOYMENT_NAME}
+yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} spec.targetService.candidate ${CANARY_VERSION}
+yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} spec.trafficControl.onSuccess ${ON_SUCCESS}
 
-yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} \
-  metadata.name ${EXPERIMENT_NAME}
-yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} \
-  spec.targetService.baseline ${BASELINE_VERSION}
-yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} \
-  spec.targetService.candidate ${CANARY_DEPLOYMENT_NAME}
-yq write --inplace ${EXPERIMENT_TEMPLATE_FILE} \
-  spec.trafficControl.onSuccess baseline
 cat ${EXPERIMENT_TEMPLATE_FILE}
 
-kubectl --namespace ${CLUSTER_NAMESPACE} \
-  apply --filename ${EXPERIMENT_TEMPLATE_FILE}
+# Create experiment in cluster
+kubectl --namespace ${CLUSTER_NAMESPACE} apply --filename ${EXPERIMENT_TEMPLATE_FILE}
