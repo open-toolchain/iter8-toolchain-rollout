1362b4ec9c6fe091203e91b76568b547fb02edcb
Michael Kalantar <kalantar@us.ibm.com>
2019-08-30 09:29:41 -0400

    create ns specific host in gw/vs

--- scripts/bookinfo_check_install.sh
+++ scripts/bookinfo_check_install.sh
@@ -27,15 +27,58 @@ kubectl --namespace ${CLUSTER_NAMESPACE} \
    apply -f https://raw.githubusercontent.com/kalantar/canary-testing-istio-toolchain/master/scripts/bookinfo.yaml
 
 # Expose bookinfo
-kubectl --namespace ${CLUSTER_NAMESPACE} \
-    apply -f https://raw.githubusercontent.com/kalantar/canary-testing-istio-toolchain/master/scripts/bookinfo-gateway.yaml
+#kubectl --namespace ${CLUSTER_NAMESPACE} \
+#    apply -f https://raw.githubusercontent.com/kalantar/canary-testing-istio-toolchain/master/scripts/bookinfo-gateway.yaml
+
+HOST="bookinfo.${CLUSTER_NAMESPACE}"
+echo "HOST=$HOST"
+
+cat <<EOF | kubectl --namespace ${CLUSTER_NAMESPACE} apply -f -
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: bookinfo-gateway
+spec:
+  selector:
+    istio: ingressgateway # use istio default controller
+  servers:
+  - port:
+      number: 80
+      name: http
+      protocol: HTTP
+    hosts:
+    - "${HOST}"
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: bookinfo
+spec:
+  hosts:
+  - "${HOST}"
+  gateways:
+  - bookinfo-gateway
+  http:
+  - match:
+    - uri:
+        exact: /productpage
+    - uri:
+        exact: /login
+    - uri:
+        exact: /logout
+    - uri:
+        prefix: /api/v1/products
+    route:
+    - destination:
+        host: productpage
+        port:
+          number: 9080
+EOF
 
 LOADBALANCER=$(kubectl --namespace istio-system get service istio-ingressgateway --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
 PORT=$(kubectl --namespace istio-system get service istio-ingressgateway --output jsonpath='{.spec.ports[?(@.targetPort==80)].nodePort}')
 APP_URL="http://$LOADBALANCER:$PORT/productpage"
+
 echo "Application URL: $APP_URL"
-
-HOST=$(kubectl --namespace ${CLUSTER_NAMESPACE} get gateway bookinfo-gateway --output jsonpath='{.spec.servers[0].hosts[0]}')
 echo "   curl command: curl -Is -H 'Host: $HOST' $APP_URL"
-
 echo "Load Generation: watch -x -n 0.1 curl -Is -H 'Host: $HOST' $APP_URL"
