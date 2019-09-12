849de7968352a4ca68f5814d620948091da1e829
Michael Kalantar <kalantar@us.ibm.com>
2019-09-12 09:26:37 -0400

    handle iter8.tools and iter8-tools in selector

--- scripts/identify_stable.sh
+++ scripts/identify_stable.sh
@@ -8,14 +8,19 @@
 # But if this isn't defined will select first deployment that satisfies
 # the service selector (service from Experiment)
 
-NUM_DR=$(kubectl --namespace ${CLUSTER_NAMESPACE} get dr --selector=iter8.tools/role=stable --output json | jq '.items | length')
+CONNECTOR='.'
+NUM_DR=$(kubectl --namespace ${CLUSTER_NAMESPACE} get dr --selector=iter8${CONNECTOR}tools/role=stable --output json | jq '.items | length')
+if (( ${NUM_DR} == 0 )); then
+  CONNECTOR='-'
+  NUM_DR=$(kubectl --namespace ${CLUSTER_NAMESPACE} get dr --selector=iter8${CONNECTOR}tools/role=stable --output json | jq '.items | length')
+fi
 if (( ${NUM_DR} == 0 )); then
   # Find deployment(s) implementing $SERVICE
   SERVICE=$(yq read ${EXPERIMENT_TEMPLATE_FILE} spec.targetService.name)
   #SERVICE=$(kubectl --namespace ${CLUSTER_NAMESPACE} get experiments.iter8.tools ${EXPERIMENT_NAME} --output jsonpath='{.spec.targetService.name}')
   DEPLOY_SELECTOR=$(kubectl --namespace ${CLUSTER_NAMESPACE} get svc ${SERVICE} --output json | jq -r '.spec.selector | to_entries[] | "\(.key)=\(.value)"' | paste -sd',' -)
 else
-  DEPLOY_SELECTOR=$(kubectl --namespace ${CLUSTER_NAMESPACE} get dr --selector=iter8.tools/role=stable -o json | jq -r '.items[0].spec.subsets[] | select(.name == "stable") | .labels | to_entries[] | "\(.key)=\(.value)"' | paste -sd',' -)
+  DEPLOY_SELECTOR=$(kubectl --namespace ${CLUSTER_NAMESPACE} get dr --selector=iter8${CONNECTOR}tools/role=stable -o json | jq -r '.items[0].spec.subsets[] | select(.name == "stable") | .labels | to_entries[] | "\(.key)=\(.value)"' | paste -sd',' -)
 fi
 echo "DEPLOY_SELECTOR=$DEPLOY_SELECTOR"
 NUM_DEPLOY=$(kubectl --namespace ${CLUSTER_NAMESPACE} get deploy --selector=${DEPLOY_SELECTOR} --output json | jq '.items | length')
