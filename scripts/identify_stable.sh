fad0eac45070071e990c9352ffc5daa9136839cd
Benoit Jaunin <jaunin.b@fr.ibm.com>
2020-07-02 15:35:30 +0200

    Adopt iter8 v0.2.1 for Tekton Flavor Pipeline (#27)
    * Add pipeline-roll and task failures option params
    
    * Reference dual-template branch for scripts invocation
    
    * Use iter8 script to perform the installations and grafana dashboard install
    
    * Update to implement the heuristic and prevent to use the first deployment available (that can be not related to the real application under experimentation)
    
    * Use .action field instead of .spec.assessment

--- scripts/identify_stable.sh
+++ scripts/identify_stable.sh
@@ -15,12 +15,17 @@ if (( ${NUM_DR} == 0 )); then
   NUM_DR=$(kubectl --namespace ${CLUSTER_NAMESPACE} get dr --selector=iter8${CONNECTOR}tools/role=stable --output json | jq '.items | length')
 fi
 if (( ${NUM_DR} == 0 )); then
-  # Find deployment(s) implementing $SERVICE
+  # No DestinationRule 
+  DEPLOY_SELECTOR=""
+else
+  # Find the stable destination rule and the associetd deployment selector
+  DEPLOY_SELECTOR=$(kubectl --namespace ${CLUSTER_NAMESPACE} get dr --selector=iter8${CONNECTOR}tools/role=stable -o json | jq -r '.items[0].spec.subsets[] | select(.name == "stable") | .labels | to_entries[] | "\(.key)=\(.value)"' | paste -sd',' -)
+fi
+if [ -z "$DEPLOY_SELECTOR" ]; then
+  # No stable DestinationRule found so find the deployment(s) implementing $SERVICE
   SERVICE=$(yq read ${EXPERIMENT_TEMPLATE_FILE} spec.targetService.name)
   #SERVICE=$(kubectl --namespace ${CLUSTER_NAMESPACE} get experiments.iter8.tools ${EXPERIMENT_NAME} --output jsonpath='{.spec.targetService.name}')
   DEPLOY_SELECTOR=$(kubectl --namespace ${CLUSTER_NAMESPACE} get svc ${SERVICE} --output json | jq -r '.spec.selector | to_entries[] | "\(.key)=\(.value)"' | paste -sd',' -)
-else
-  DEPLOY_SELECTOR=$(kubectl --namespace ${CLUSTER_NAMESPACE} get dr --selector=iter8${CONNECTOR}tools/role=stable -o json | jq -r '.items[0].spec.subsets[] | select(.name == "stable") | .labels | to_entries[] | "\(.key)=\(.value)"' | paste -sd',' -)
 fi
 echo "DEPLOY_SELECTOR=$DEPLOY_SELECTOR"
 NUM_DEPLOY=$(kubectl --namespace ${CLUSTER_NAMESPACE} get deploy --selector=${DEPLOY_SELECTOR} --output json | jq '.items | length')
